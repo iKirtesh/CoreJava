bab03e56b9b43dc6b7a8a8b873a53e2bc6052d7b
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-10-19 08:27:27 +0530

    Final Word in java

---
+++ src/Core_Java/Final/Final.java
@@ -0,0 +1,9 @@
+package Core_Java.Final;
+
+public class Final {
+    public static void main(String[] args) {
+        final int number = 10;
+        // number = 20;    // Error: cannot assign a value to final variable number
+        System.out.println("Number : " + number);
+    }
+}
