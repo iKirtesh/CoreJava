7616440656363f6dddc15563b65fca2ef43d3293
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-10-21 00:19:47 +0530

    Enum in java

--- src/Core_Java/Enum/Enum.java
+++ src/Core_Java/Enum/Enum.java
@@ -5,6 +5,20 @@ enum Color {
 }
 public class Enum {
     public static void main(String[] args) {
+        // Accessing enum constants
+        Color c1 = Color.RED;
+        Color c2 = Color.GREEN;
+
+        // Using values() method to iterate through all enum constants
+        for (Color color : Color.values()) {
+            System.out.println(color);
+        }
+
+        // Using valueOf() method to get an enum constant by name
+        Color c3 = Color.valueOf("BLUE");
+        System.out.println("c3 is " + c3);
+    }
+    public static void main1(String[] args) {
         System.out.println("Name : " + Color.RED.name());
         System.out.println("Ordinal : " + Color.RED.ordinal());
     }
