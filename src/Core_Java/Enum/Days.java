995bb9b28f3dc649119abc0c307c844b41fbbe96
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-10-21 11:43:27 +0530

    Enum in java

---
+++ src/Core_Java/Enum/Days.java
@@ -0,0 +1,21 @@
+package Core_Java.Enum;
+
+enum Day {
+    SUN("Sunday"), MON("Monday"), TUE("Tuesday");
+    private String dayName;
+    Day (String dayName) {
+        this.dayName = dayName;
+    }
+    public String getDayName() {
+        return dayName;
+    }
+}
+
+class Program {
+    public static void main(String[] args) {
+    Day[] days = Day.values();
+        for (Day day : days) {
+            System.out.println(day + " : " + day.getDayName());
+        }
+    }
+}
