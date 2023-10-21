1cd22cc9e7691d72d56df3b6ba77ab967f11afdf
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-10-21 22:01:21 +0530

    Association in Java

---
+++ src/Core_Java/Association/Program.java
@@ -0,0 +1,48 @@
+class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Book {
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
+
+class Library {
+    private Person librarian;
+    private Book book;
+
+    public Library(Person librarian, Book book) {
+        this.librarian = librarian;
+        this.book = book;
+    }
+
+    public void displayInfo() {
+        System.out.println("Librarian: " + librarian.getName());
+        System.out.println("Book Title: " + book.getTitle());
+    }
+}
+
+public class Program {
+    public static void main(String[] args) {
+        Person librarian = new Person("Alice");
+        Book book = new Book("Java Programming 101");
+
+        Library library = new Library(librarian, book);
+        library.displayInfo();
+    }
+}
---
+++ src/Core_Java/Date/LocalDates.java
@@ -0,0 +1,13 @@
+package Core_Java.Date;
+
+import java.time.LocalDate;
+
+public class LocalDates {
+    public static void main(String[] args) {
+        LocalDate ldt = LocalDate.now();
+        int day = ldt.getDayOfMonth();
+        int month = ldt.getMonthValue();
+        int year = ldt.getYear();
+        System.out.println(day+" / "+month+" / "+year);
+    }
+}
