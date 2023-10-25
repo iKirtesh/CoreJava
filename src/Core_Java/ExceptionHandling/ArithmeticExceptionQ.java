1ca059b02a4e52bf07ff9142133b596a59c1328f
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-10-26 03:07:56 +0530

    try catch block in Exception Handling (Java)

--- src/Core_Java/ExceptionHandling/ArithmeticExceptionQ.java
+++ src/Core_Java/ExceptionHandling/ArithmeticExceptionQ.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 
 public class ArithmeticExceptionQ {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Resource is allocated");
-        System.out.print("Enter a number: ");
-        int num1 = in.nextInt();
-        System.out.print("Enter another number: ");
-        int num2 = in.nextInt();
 
+        Scanner  in = new Scanner(System.in);
         try {
+            System.out.println("Resource is allocated");
+            System.out.print("Enter a number: ");
+            int num1 = in.nextInt();
+            System.out.print("Enter another number: ");
+            int num2 = in.nextInt();
+
             int result = num1 / num2;
             System.out.println("Result: " + result);
         } catch (ArithmeticException e) {
+            System.out.println("Exception: " + e.getMessage());
+        } catch (InputMismatchException e) {
             System.out.println("Exception: " + e.getMessage());
         }
 
