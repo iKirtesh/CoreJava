456673a73dc75ebb46ca2361bf7ec795bc130a66
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-10-25 16:07:20 +0530

    Exception Handling

---
+++ src/Core_Java/ExceptionHandling/ArithmeticExceptionQ.java
@@ -0,0 +1,24 @@
+package Core_Java.ExceptionHandling;
+
+import java.util.Scanner;
+
+public class ArithmeticExceptionQ {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Resource is allocated");
+        System.out.print("Enter a number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter another number: ");
+        int num2 = in.nextInt();
+
+        if (num2 == 0) {
+            System.out.println("Cannot divide by zero");
+            System.exit(0);
+        }
+
+        int result = num1 / num2;
+        System.out.println("Result: " + result);
+        in.close();
+        System.out.println("Resource is released / deallocated");
+    }
+}
