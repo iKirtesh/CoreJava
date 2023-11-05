d15a830f92d2292963b7ad94bef5d1def434e1c2
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-11-06 01:08:56 +0530

    Adapter Pattern in Design Patterns

---
+++ src/Design_Patterns/Structural_Patterns/Adapter/Main.java
@@ -0,0 +1,65 @@
+package Design_Patterns.Structural_Patterns.Adapter;
+
+interface AndroidCharger {
+    void chargeAndroidPhone();
+}
+
+class ChargerABC implements AndroidCharger {
+
+    @Override
+    public void chargeAndroidPhone() {
+        System.out.println("Your android phone charging");
+    }
+}
+
+
+interface AppleCharger {
+    void chargePhone();
+}
+
+class ChargerXYZ implements AppleCharger {
+
+    @Override
+    public void chargePhone() {
+        System.out.println("Your iphone charging");
+    }
+}
+
+
+class IPhone15 {
+    private AppleCharger appleCharger;
+
+    public IPhone15(AppleCharger appleCharger) {
+        this.appleCharger = appleCharger;
+    }
+
+    public void chargeIphone() {
+        appleCharger.chargePhone();
+    }
+}
+
+class Adapter implements AppleCharger {
+    private final AndroidCharger charger;
+
+    public Adapter(AndroidCharger charger) {
+        this.charger = charger;
+    }
+
+    @Override
+    public void chargePhone() {
+        charger.chargeAndroidPhone();
+        System.out.println("Your Phone is charging with adapter");
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program Started");
+        
+        AppleCharger charger = new Adapter(new ChargerABC());
+
+        IPhone15 iPhone15 = new IPhone15(charger);
+        iPhone15.chargeIphone();
+    }
+}
