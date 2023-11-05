4bb52ae9a95552c3c18f8d08df09d7a51657a28e
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-11-05 16:11:10 +0530

    Builder in Design Patterns

---
+++ src/Design_Patterns/Builder/User.java
@@ -0,0 +1,83 @@
+package Design_Patterns.Builder;
+
+class User {
+    private final String userId;
+    private final String userName;
+    private final String emailId;
+
+    private User(UserBuilder builder) {
+        this.userId = builder.userId;
+        this.userName = builder.userName;
+        this.emailId = builder.emailId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    @Override
+    public String toString() {
+        return this.userId + " " + this.userName + " " + this.emailId;
+    }
+
+
+    // Inner class to create a User object
+    static class UserBuilder {
+        private String userId;
+        private String userName;
+        private String emailId;
+
+        public UserBuilder() {
+        }
+
+        public static UserBuilder newInstance() {
+            return new UserBuilder();
+        }
+
+        public UserBuilder setUserId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public UserBuilder setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public UserBuilder setEmailId(String emailId) {
+            this.emailId = emailId;
+            return this;
+        }
+
+        public User build() {
+            User user = new User(this);
+            return user;
+        }
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        User user = new User.UserBuilder()
+                .setUserId("12")
+                .setUserName("iKirtesh")
+                .setEmailId("kirteshadmute@gmail.com")
+                .build();
+        System.out.println(user);
+
+        User user2 = User.UserBuilder.newInstance()
+                .setUserId("13")
+                .setUserName("iKirtesh")
+                .setEmailId("kirtesh@gmail.com")
+                .build();
+        System.out.println(user2);
+    }
+}
