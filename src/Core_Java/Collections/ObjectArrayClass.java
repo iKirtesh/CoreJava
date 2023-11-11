2de3f51fdc21e963d5129220f6a3b32756b077cb
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-11-11 10:42:47 +0530

    Introduction to Collection Framework

---
+++ src/Core_Java/Collections/ObjectArrayClass.java
@@ -0,0 +1,16 @@
+package Core_Java.Collections;
+
+import java.util.Objects;
+
+public class ObjectArrayClass {
+    public static void main(String[] args) {
+        Object[] objectArray = new Object[3];
+        objectArray[0] = "Java";
+        objectArray[1] = 42;
+        objectArray[2] = new ObjectArrayClass();
+
+        for (Object obj : objectArray) {
+            System.out.println(obj);
+        }
+    }
+}
