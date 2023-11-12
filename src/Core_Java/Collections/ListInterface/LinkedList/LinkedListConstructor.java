f87c8f99cdbf6e5f348b25ea82398df37a6ee5ad
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-11-12 07:25:53 +0530

    LinkedList in Collection Framework

---
+++ src/Core_Java/Collections/ListInterface/LinkedList/LinkedListConstructor.java
@@ -0,0 +1,21 @@
+package Core_Java.Collections.ListInterface.LinkedList;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListConstructor {
+    public static void main(String[] args) {
+        // Default constructor
+        LinkedList<String> ll = new LinkedList<String>();
+
+        // Constructor from Collection
+        List<String> list = Arrays.asList("Kirtesh", "Saurabh", "Shubham", "Pratik");
+        LinkedList<String> ll1 = new LinkedList<String>(list);
+        System.out.println(ll1);
+
+        // Constructor from another LinkedList
+        LinkedList<String> ll2 = new LinkedList<String>(ll1);
+        System.out.println(ll2);
+    }
+}
---
+++ src/Core_Java/Collections/ListInterface/LinkedList/LinkedLists.java
@@ -0,0 +1,20 @@
+package Core_Java.Collections.ListInterface.LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedLists {
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<String>();
+        ll.add("Kirtesh");
+        ll.add("Saurabh");
+        ll.add("Shubham");
+        ll.add("Pratik");
+
+        System.out.println(ll);
+
+        // Iterating over linked list
+        for (String str : ll) {
+            System.out.println(str);
+        }
+    }
+}
