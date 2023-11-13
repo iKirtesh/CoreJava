edf4711e9285d400c528e37192d1e26c75fd7f48
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-11-13 20:11:47 +0530

    *Properties Class in Collection Framework

---
+++ src/Core_Java/CollectionsFramework/Properties/PropertiesClass.java
@@ -0,0 +1,20 @@
+package Core_Java.CollectionsFramework.Properties;
+
+import java.util.Properties;
+
+public class PropertiesClass {
+    public static void main(String[] args) {
+        Properties p = new Properties();
+        p.setProperty("name", "Kirtesh");
+        p.setProperty("email", "kirteh3000@email.com");
+        p.setProperty("phone", "1234567890");
+
+        String name = p.getProperty("name");
+        String email = p.getProperty("email");
+        String phone = p.getProperty("phone");
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(phone);
+    }
+}
