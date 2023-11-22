807987584c2c6426efabee407880252e6bc66f04
ikirtesh <kirtesh.vsyut21@sinhgad.edu>
2023-11-22 23:58:17 +0530

    Command Pattern of Behavioral in Design Pattern

---
+++ src/Design_Patterns/Behavioral_Patterns/Command/Client.java
@@ -0,0 +1,70 @@
+package Design_Patterns.Behavioral_Patterns.Command;
+
+interface Command {
+    void execute();
+}
+
+class TubeLight {
+    public void on() {
+        System.out.println("TubeLight is on");
+    }
+    public void off() {
+        System.out.println("TubeLight is off");
+    }
+}
+
+class TubeLightOnCommand implements Command {
+    private TubeLight tubeLight;
+
+    public TubeLightOnCommand(TubeLight tubeLight) {
+        this.tubeLight = tubeLight;
+    }
+
+    @Override
+    public void execute() {
+        tubeLight.on();
+    }
+}
+
+class TubeLightOffCommand implements Command {
+    private TubeLight tubeLight;
+
+    public TubeLightOffCommand(TubeLight tubeLight) {
+        this.tubeLight = tubeLight;
+    }
+
+    @Override
+    public void execute() {
+        tubeLight.off();
+    }
+}
+
+class RemoteController {
+    private Command command;
+
+    public RemoteController(Command command) {
+        this.command = command;
+    }
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
+
+public class Client {
+    public static void main(String[] args) {
+        TubeLight tubeLight = new TubeLight();
+        TubeLightOnCommand tubeLightOnCommand = new TubeLightOnCommand(tubeLight);
+        TubeLightOffCommand tubeLightOffCommand = new TubeLightOffCommand(tubeLight);
+
+        RemoteController remoteController = new RemoteController(tubeLightOnCommand);
+        remoteController.pressButton();
+
+        remoteController.setCommand(tubeLightOffCommand);
+        remoteController.pressButton();
+    }
+}
